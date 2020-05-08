e6db6e5ade5b727f52bb3e193520d8a10b70d82a
yannamsiva <yannamsiva@gmail.com>
2020-05-08 19:38:25 +0530

    first commit

---
+++ ShivaStudentWebEx/src/main/java/com/shiva/student/StudentServlet.java
@@ -0,0 +1,36 @@
+package com.shiva.student;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StudentServlet
+ */
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+PrintWriter pw=response.getWriter();
+pw.println("<h1>Welcome Shiva Devops Training@9985538462</h1>");
+
+	
+	}
+
+}
---
+++ ShivaStudentWebEx/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+
+
+<h1>Welcome To Shiva Develops Training</h1>
+
+<a href="StudentServlet">Click</a>
